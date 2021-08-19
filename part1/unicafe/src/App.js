9e5209540c5241071db50d98068e698d091c3969
scorztv <sorzech@me.com>
2021-08-19 18:11:47 +0800

    clean up ^^;

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -26,18 +26,18 @@ const Statistics = ({good, neutral, bad}) => {
     )
   } 
   return (
-      <div>
-        <table>
-          <tbody>
-            <StatisticLine text="good" value={good} />
-            <StatisticLine text="neutral" value={neutral} />
-            <StatisticLine text="bad" value={bad} />
-            <StatisticLine text="all" value={all} />
-            <StatisticLine text="average" value={average} />
-            <StatisticLine text="positive" value={positive} />
-          </tbody>
-        </table>
-      </div>
+    <div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
+    </div>
   )
 }
 
