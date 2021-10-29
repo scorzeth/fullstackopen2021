6fd1bb6e4f2e4f894ad66b52c4a347f0299a4992
scorztv <sorzech@me.com>
2021-10-29 16:22:52 +0800

    Exercise 5.7 - can now show and hide details of the blogs

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button style={hideWhenVisible} onClick={toggleVisibility}>View</button>
+        <button style={showWhenVisible} onClick={toggleVisibility}>Hide</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url}<br />
+        Likes {blog.likes} <button>Like</button> <br />
+        {blog.user.name}<br />
+      </div>
+    </div>  
+)}
 
 export default Blog
